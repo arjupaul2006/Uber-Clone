edf525393ca64c66125f74a0b4b96b4c8b2e5b4d
Arju Paul <arjupaul2006@gmail.com>
2026-01-16 19:27:38 +0530

    user login done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,3 +27,31 @@ module.exports.registerUser = async (req, res, next) => {
 
   res.status(201).json({ user, token });
 };
+
+module.exports.loginUser = async (req, res, next) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(400).json({ errors: error.array() });
+  }
+
+  const { email, password } = req.body;
+
+  // find user by email
+  const user = await userModel.findOne({ email }).select("+password");
+
+  // check if user exists
+  if (!user) {
+    return res.status(401).json({ message: "User not found" });
+  }
+
+  // compare password
+  const isPasswordValid = await user.comparePassword(password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
+
+  // generate auth token
+  const token = user.generateAuthToken();
+
+  res.status(200).json({ user, token });
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/userController");
 
+// signup route
 router.post(
   "/register",
 
@@ -22,7 +23,26 @@ router.post(
       .isLength({ min: 6 })
       .withMessage("Password must be at least 6 characters long"),
   ],
+  
   userController.registerUser
+);
+
+// login route
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please provide a valid email address")
+      .isLength({ min: 5 })
+      .withMessage("Email must be at least 5 characters long"),
+
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+
+  userController.loginUser
 );
 
 module.exports = router;
